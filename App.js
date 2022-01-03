565fe1ea699e434d0c44facf2f9d0c8333c68cac
RakibRahman <rakibrahman934@gmail.com>
2022-01-03 09:50:28 +0600

    new todo items appended to list

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-const onSubmit = document.getElementById("submit");
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
-onSubmit.addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
-    const variable = event.target.value;
-    console.log(variable);
-  }
-});
-onSubmit.addEventListener("blur", (event) => {
-  event.target.value = "";
-});
+//Event Listeners
+todoButton.addEventListener("click", addToDo);
+
+//Functions
+function addToDo(e) {
+  e.preventDefault();
+  //Create todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create li items
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  //Stick the li inside of div
+  todoDiv.appendChild(newTodo);
+  //Create completed button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class='fas fa-check'></i>";
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+  //Create delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+  //Sticking the new todos inside of the list
+  todoList.appendChild(todoDiv);
+}
